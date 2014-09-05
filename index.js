83c4c03fb02afa731c87cd567e81f4e776e08498
Gordon Hall <gordon@gordonwritescode.com>
2014-09-04 22:59:47 -0400

    refactor index

--- index.js
+++ index.js
@@ -5,17 +5,15 @@
  * Initializes module and exposes public methods
  */
 
-var Repository = require('./lib/repository')
-  , Command = require('./lib/command')
-  , pty = require('pty.js')
-  , gitty;
+var Repository = require('./lib/repository');
+var pty        = require('pty.js');
 
 /**
  * Setup function for getting access to a GIT repo
  * @constructor
  * @param  {String} path
  */
-gitty = function(path) {
+var Gitty = function(path) {
 	return new Repository(path);
 };
 
@@ -25,53 +23,54 @@ gitty = function(path) {
  * @param  {String}   val
  * @param  {Function} callback
  */
-function config(key, val, callback) {
-	var gitConfig = new Command(__dirname, 'config', ['--global', key], '"' + val + '"');
-	gitConfig.exec(function(error, stdout, stderr) {
-		var err = error || stderr;
-		if (callback) {
-			callback.call(this, err);
-		}
-	});
+Gitty.config = function(key, val, callback) {
+  var cmd = new Command('/', 'config', ['--global', key], '"' + val + '"');
+  var done = callback || new Function();
+
+  cmd.exec(function(error, stdout, stderr) {
+    done(error || stderr || null);
+  });
 };
 
 /**
  * Wrapper for the GIT clone function
  * @param  {String}   path
  * @param  {String}   url
- * @param  {Function} callback
  * @param  {Object}   creds
+ * @param  {Function} callback
  */
-function clone(path, url, callback, creds) {
-	var pterm = pty.spawn('git', ['clone', url, path], { cwd : path })
-	  , repo = this
-	  , err
-	  , succ;
-	pterm.on('data', function(data) {
-		var prompt = data.toLowerCase();
-		if (prompt.indexOf('username') > -1) {
-			pterm.write(creds.user + '\r');
-		} else if (prompt.indexOf('password') > -1) {
-			pterm.write(creds.pass + '\r');
-		} else if ((prompt.indexOf('error') > -1) || (prompt.indexOf('fatal') > -1)) {
-			err = prompt;
-		} else {
-			succ = prompt;
-		}
-	});
-	pterm.on('exit', function() {
-		callback.call(repo, err, succ);
-	});
-};
+Gitty.clone = function(path, url) {
+  var self  = this;
+  var args  = Array.prototype.slice.apply(arguments);
+  var creds = args[2].username ? args[2] || {};
+  var done  = args.slice(-1).pop() || new Function();
+  var pterm = pty.spawn('git', ['clone', url, path], { cwd : path });
+  var error = null;
 
-gitty.config = config;
-gitty.clone = clone;
-gitty.Repository = Repository;
-gitty.Command = Command;
+  pterm.on('data', function(data) {
+    var prompt = data.toLowerCase();
+
+    if (prompt.indexOf('username') > -1) {
+      return pterm.write(creds.username + '\r');
+    }
+
+    if (prompt.indexOf('password') > -1) {
+      return pterm.write(creds.password + '\r');
+    }
+
+    if ((prompt.indexOf('error') > -1) || (prompt.indexOf('fatal') > -1)) {
+      return error = prompt;
+    }
+  });
+
+  pterm.on('exit', function() {
+    callback(error);
+  });
+};
 
 /**
  * Export Contructor
  * @constructor
  * @type {Object}
  */
-module.exports = gitty;
+module.exports = Gitty;
